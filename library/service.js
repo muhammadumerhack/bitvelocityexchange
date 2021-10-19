a2b75db868edbdec4163a730f0d14904ab3a4158
Muhammad Umer Farooqi <umer1807f@aptechsite.net>
2021-10-20 00:25:30 +0500

    Wrong File Uploaded Fixed

--- library/service.js
+++ library/service.js
@@ -1,9 +1,12 @@
 const chalk = require('chalk');
 const axios = require('axios');
+var {prompt} = require('inquirer');
 const ui = require('cliui')()
-var exchangeRate;
-var adminBalance = 100;
-var userBalance = 5000000;
+var exchangeRate = 65015;
+let adminBalance = 100;
+let adminUSDBalance = 0;
+let userBTCBlance = 0;
+let userBalance = 5000000;
 
 let history = [];
 let buyers ={}
@@ -32,21 +35,23 @@ let usdToBtcConverter = (usd) =>{
 }
 let updateBalance =(value) =>{
         let btc = usdToBtcConverter(value);
-        adminBalance - btc;
-        userBalance + value;
+        adminBalance -= btc;
+        adminUSDBalance += value;
+        userBalance -= value;
+        userBTCBlance += btc
 }
 let updateHistory = (role,matched,value,quantity)=>{
     let details,description = "";
-    if(matched){
-        description += ' ( Order matched)';
-    }if(role=="buy"){
-        details += 'Buy Price set for $'+ value;
-        description += 'Buy order placed for'+ quantity + ' btc at $'+ value
+    if(role=="buy"){
+        details = 'Buy Price set for $'+ value;
+        description = 'Buy order placed for '+ quantity + ' btc at $'+ value
     }if(role=="sell"){
-        details += 'Sell Price set for $'+ value;
-        description += 'Sell order placed for'+ quantity + ' btc at $'+ value
+        details = 'Sell Price set for $'+ value;
+        description = 'Sell order placed for '+ quantity + ' btc at $'+ value;
     }
-    updateBalance(value);
+    if(matched===true){
+        description += ' ( Order matched)';
+    }
     history.push({details,description});
     
 }
@@ -58,6 +63,8 @@ let match = (price,array,cb)=>{
             array.splice(item, 1);
             let value = temp.value;
             let quantity = temp.quantity;
+            
+            updateBalance(value);
             cb(true,value,quantity)
         }else {
             cb(false,null,null)
@@ -67,12 +74,10 @@ let match = (price,array,cb)=>{
 let sell = (quantity, value) =>{
     y++;
     sellers.push({y,quantity,value});
+    updateHistory("sell",false,value, quantity) 
     match(value,buyers,(matched,value,quantity)=>{
         if(matched){
-            updateHistory(sell,matched,value, quantity)
-        }
-        if(!matched){
-            updateHistory(sell,matched,value, quantity) 
+            updateHistory("sell",true,value, quantity)
         }
     })
      
@@ -80,25 +85,161 @@ let sell = (quantity, value) =>{
 let buy = (quantity, value) =>{
     buyers +={x,quantity,value};
     x++;
+    updateHistory("buy",false,value, quantity) 
     match(value,sellers,(matched,value,quantity)=>{
         if(matched){
-            updateHistory(buy,matched,value, quantity)
-        }
-        if(!matched){
-            updateHistory(buy,matched,value, quantity) 
+            updateHistory("buy",true,value, quantity)
         }
     })
 }  
 
 let displaytable =()=>{
+ ui.div({
+    text: chalk.bold.yellow('BITCOIN EXCHANGE 2021'),
+    align: "center",
+    padding: [2, 0, 1, 0],
+ })
+ ui.div({
+    text: 'Admin BTC Balance' ,
+    align: "center",
+    padding: [1, 0, 1, 0],
+ },
+ {
+    text: chalk.bold.green('BTC '+adminBalance) ,
+    align: "center",
+    padding: [1, 0, 1, 0],
+ },
+ {
+    text: 'User Balance',
+    align: "center",
+    padding: [1, 0, 1, 0],
+ }, {
+    text: chalk.bold.green('$ '+ userBalance),
+    align: "center",
+    padding: [1, 0, 1, 0],
+ }
+ )
+ ui.div({
+    text: 'Admin USD Balance' ,
+    align: "center",
+    padding: [1, 0, 1, 0],
+ },
+ {
+    text: chalk.bold.greenBright('$ '+ adminUSDBalance) ,
+    align: "center",
+    padding: [1, 0, 1, 0],
+ },
+ {
+    text: 'User BTC Balance',
+    align: "center",
+    padding: [1, 0, 1, 0],
+ }, {
+    text: chalk.bold.greenBright('BTC '+userBTCBlance),
+    align: "center",
+    padding: [1, 0, 1, 0],
+ }
+ )
+ ui.div({
+    text: chalk.italic.blueBright('BTC/USD: $ '+ exchangeRate),
+    align: "center",
+    padding: [2, 0, 2, 0],
+    
+ })
 
+ 
+ if(history[0]===undefined) {
+    ui.div({
+        text: 'No Transactions Processed',
+        padding: [1, 0, 2, 0],
+        align: 'center',
+        border: "__"
+     })
+ }
+ else if(history!==[]){
+    for(let i in history){
+        ui.div(
+            {
+                text: i,
+                padding: [1, 1, 1, 1],
+                align: 'center'
+            },
+            {
+                text:  history[i].details,
+                padding: [1, 1, 1, 1],
+                align: 'center'
+            },
+            {
+                text:  history[i].description,
+                padding: [1, 1, 1, 1],
+                align: 'center'
+            },
+        )
+         
+    }
+ }
+ 
+ console.log(ui.toString())
+ takeInput()
 }
+let takeInput = () => {
+    prompt([
+        {
+        name : 'option',    
+        type: 'number',
+        message : "Want To Buy or Sell  -Enter 1 to buy and 2 to Sell",
+        }
+      ])
+      .then((answers) => {
+        if(answers.option == 1){
+            prompt([
+                {
+                name : 'val',    
+                type: 'number',
+                message : chalk.red("Enter the Price at which you want to Buy")
+                },
+                { 
+                name : 'qua',    
+                type: 'number',
+                message : chalk.red("How many BTC you want to Buy")
+                }
+              ])
+              .then((answers) => {
+                buy(answers.qua,answers.val)
+                displaytable()
+              })
+        }else if(answers.option == 2){
+                prompt([
+                    {
+                    name : 'val',    
+                    type: 'number',
+                    message : chalk.red("Enter the Price at which you want to Sell")
+                    },
+                    { 
+                    name : 'qua',    
+                    type: 'number',
+                    message : chalk.red("How many BTC you want to Sell")
+                    }
+                ])
+                .then((answers) => {
+                    sell(answers.qua,answers.val)
+                    displaytable()
+                })
+        }else {
+            takeInput();
+        }
+      })
+ }
+displaytable()
+
+
+
+
 // Refresh Every 5 min
 setInterval(() => {
 //On Server Restart
 if(exchangeRate == null){ 
-  getExchangeRate()
+  getExchangeRate();
 }else{
     getExchangeRate();
   }
-}, 1000);
+}, 10000);
